5ed492eb9ebc9098214f739a6d517d606903d319
Manoj Maharjan <manoj.maharjan@hotmail.com>
2025-03-27 12:37:58 +0100

    Added tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx}", // adjust this based on your file locations
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("daisyui"), // Add DaisyUI plugin here
+  ],
+};
