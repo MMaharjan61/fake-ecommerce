229f59d2ac98224b890150439b3a08221ea9c9f7
Manoj Maharjan <manoj.maharjan@hotmail.com>
2025-03-30 21:12:47 +0200

    Added product image and updated styling

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -18,7 +18,7 @@ const CartPage = () => {
   }
 
   return (
-    <div className="p-2 mx-44">
+    <div className="mx-120">
       {total === 0 ? (
         <div>
           <p className="p-2 flex justify-center font-semibold text-xl text-black">
@@ -26,54 +26,63 @@ const CartPage = () => {
           </p>
         </div>
       ) : (
-        <table className="table mt-2 w-full">
-          <thead>
-            <tr>
-              <th className="text-black text-lg">Product</th>
-              <th className="text-black text-lg">Price</th>
-              <th className="text-black text-lg">Quantity</th>
-              <th className="text-black text-lg">Total</th>
-              <th className="text-black text-lg">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {cart.map((item) => (
-              <tr key={item.id}>
-                <td>{item.title}</td>
-                <td>{item.price} €</td>
-                <td>
-                  <button
-                    className="btn btn-soft btn-circle mx-2"
-                    onClick={() => decreaseQuantity(item.id, cart, setCart)}
-                  >
-                    -
-                  </button>
-                  {item.quantity}
-                  <button
-                    className="btn btn-soft btn-circle mx-2"
-                    onClick={() => increaseQuantity(item.id, cart, setCart)}
-                  >
-                    +
-                  </button>
-                </td>
-                <td>{(item.price * item.quantity).toFixed(2)} €</td>
-                <td>
-                  <button
-                    className="btn btn-soft"
-                    onClick={() => removeFromCart(item.id, cart, setCart)}
-                  >
-                    Remove
-                  </button>
-                </td>
+        <div className="absolute">
+          <table className="table mt-2 w-full">
+            <thead>
+              <tr>
+                <th className="text-black text-lg">Product</th>
+                <th className="text-black text-lg">Price</th>
+                <th className="text-black text-lg">Quantity</th>
+                <th className="text-black text-lg">Total</th>
+                <th className="text-black text-lg">Action</th>
               </tr>
-            ))}
-          </tbody>
-          <div className="p-4">
-            <button className="btn btn-accent">
-              Checkout: {total.toFixed(2)} €
-            </button>
-          </div>
-        </table>
+            </thead>
+            <tbody>
+              {cart.map((item) => (
+                <tr key={item.id}>
+                  <td className="flex items-center">
+                    <img
+                      src={item.image}
+                      alt={item.title}
+                      className="w-16 h-16 mr-4 image-sharp object-contain"
+                    />
+                    <div className="w-100 overflow-hidden">{item.title}</div>
+                  </td>
+                  <td>{item.price} €</td>
+                  <td>
+                    <button
+                      className="btn btn-soft btn-circle mx-2"
+                      onClick={() => decreaseQuantity(item.id, cart, setCart)}
+                    >
+                      -
+                    </button>
+                    {item.quantity}
+                    <button
+                      className="btn btn-soft btn-circle mx-2"
+                      onClick={() => increaseQuantity(item.id, cart, setCart)}
+                    >
+                      +
+                    </button>
+                  </td>
+                  <td>{(item.price * item.quantity).toFixed(2)} €</td>
+                  <td>
+                    <button
+                      className="btn btn-soft"
+                      onClick={() => removeFromCart(item.id, cart, setCart)}
+                    >
+                      Remove
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+            <div className="p-4">
+              <button className="btn btn-accent">
+                Checkout: {total.toFixed(2)} €
+              </button>
+            </div>
+          </table>
+        </div>
       )}
     </div>
   );
